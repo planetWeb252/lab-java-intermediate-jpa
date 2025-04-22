ac8700d9439af2a4af7207646f142424f6eef3f6
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-22 18:49:30 +0200

    initial commit

---
+++ src/main/java/com/intermediate_labJpa/labJpaIntermediate/LabJpaIntermediateApplication.java
@@ -0,0 +1,13 @@
+package com.intermediate_labJpa.labJpaIntermediate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJpaIntermediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJpaIntermediateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intermediate_labJpa/labJpaIntermediate/LabJpaIntermediateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intermediate_labJpa.labJpaIntermediate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJpaIntermediateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
